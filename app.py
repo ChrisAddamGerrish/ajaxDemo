2134d4f11ee8feca1ae4b6ed116b337db6557ae3
Chris Gerrish <c3gerris@gmail.com>
2025-04-08 19:16:26 -0400

    Added some validation logic and reformated the code.

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def index():  # put application's code here
+def index():
     return render_template('index.html')
+
 
 @app.route('/userData', methods=['POST'])
 def returnData():
     data = request.get_json()
 
-
     if data.get('userText'):
         data['userText'] = data['userText'].upper()
 
-
         return render_template('data.html', data=data)
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,18 +1,34 @@
+function isASCII(str) {
+    return /^[\x00-\x7F]*$/.test(str);
+}
+
 function sendText() {
-    var usertext = document.getElementById("userText").value;
+    const usertext = document.getElementById("userText").value;
     const updateData = document.getElementById("result");
-    console.log(usertext);
 
-    fetch("/userData" ,
-        {
-        method: "POST",
-        headers : {'Content-Type': "application/json"},
-        body: JSON.stringify({ userText: usertext }),
+    if (!usertext.length > 0) {
+        alert('The textbox must not be empty.');
+    } else {
+
+        if (isASCII(usertext)) {
+
+            console.log(usertext);
+
+            fetch("/userData",
+                {
+                    method: "POST",
+                    headers: {'Content-Type': "application/json"},
+                    body: JSON.stringify({userText: usertext}),
+                }
+            ).then(response => {
+                return response.text();
+            })
+                .then(html => updateData.innerHTML = html);
+
+        } else {
+            alert('The textbox value contains non-ASCII characters.');
         }
-    ).then(response => {return response.text();})
-        .then(html => updateData.innerHTML=html);
-
-
-};
+    }
+}
 
 
